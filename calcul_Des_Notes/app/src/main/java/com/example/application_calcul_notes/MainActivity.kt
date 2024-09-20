f76accfbdbacaa79fd2a51497bbc4a20578da6b1
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2024-09-20 20:10:34 +0100

    fixBug

--- calcul_Des_Notes/app/src/main/java/com/example/application_calcul_notes/MainActivity.kt
+++ calcul_Des_Notes/app/src/main/java/com/example/application_calcul_notes/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.application_calcul_notes
 import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -24,47 +25,48 @@ class MainActivity : AppCompatActivity() {
         }
         val btnC = findViewById<Button>(R.id.btn1)
 
-
-
-
-            @SuppressLint("Range")
-            fun mention(a:Float){
-                val Mention = findViewById<TextView>(R.id.mention)
-                if (a < 10)
-                {
-                    Mention.text = "Échec"
-                    Mention.setTextColor(Color.parseColor("#FF0000"))
-                }
-                else if(a < 12)
-                {
-                    Mention.text = "Assez bien"
-                    Mention.setTextColor(Color.parseColor("#EEE8AA"))
-                }
-                else if(a < 14)
-                {
-                    Mention.text = "bien"
-                    Mention.setTextColor(Color.parseColor("#FFFF00"))
-                }
-                else if(a < 16)
-                {
-                    Mention.text = "tres bien"
-                    Mention.setTextColor(Color.parseColor("#008000"))
-                }
-                else
-                {
-                    Mention.text = "Excellent"
-                    Mention.setTextColor(Color.parseColor("#008000"))
-                }
+        @SuppressLint("Range")
+        fun mention(a: Float) {
+            val Mention = findViewById<TextView>(R.id.mention)
+            if (a < 10) {
+                Mention.text = "Échec"
+                Mention.setTextColor(Color.parseColor("#FF0000"))
+            } else if (a < 12) {
+                Mention.text = "Assez bien"
+                Mention.setTextColor(Color.parseColor("#EEE8AA"))
+            } else if (a < 14) {
+                Mention.text = "bien"
+                Mention.setTextColor(Color.parseColor("#FFFF00"))
+            } else if (a < 16) {
+                Mention.text = "tres bien"
+                Mention.setTextColor(Color.parseColor("#008000"))
+            } else {
+                Mention.text = "Excellent"
+                Mention.setTextColor(Color.parseColor("#008000"))
             }
+        }
+
+        btnC.setOnClickListener {
+            val note_1 = findViewById<EditText>(R.id.ip1).text.toString().trim()
+            val note_2 = findViewById<EditText>(R.id.ip2).text.toString().trim()
+            val note_3 = findViewById<EditText>(R.id.ip3).text.toString().trim()
+            val note_4 = findViewById<EditText>(R.id.ip4).text.toString().trim()
+            val TxtR = findViewById<TextView>(R.id.Result)
 
 
-            btnC.setOnClickListener{
-                val note_1 = findViewById<EditText>(R.id.ip1).text.toString()
-                val note_2 = findViewById<EditText>(R.id.ip2).text.toString()
-                val note_3 = findViewById<EditText>(R.id.ip3).text.toString()
-                val note_4 = findViewById<EditText>(R.id.ip4).text.toString()
+            fun checkisEmty(): Int {
 
-                val TxtR = findViewById<TextView>(R.id.Result)
+
+                if (note_1.isEmpty() || note_2.isEmpty() || note_3.isEmpty() || note_4.isEmpty()) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "un input ou tous les input sont vide", Toast.LENGTH_SHORT
+                    ).show();
+                    return 0;
+                }
+                return 1;
+            }
+            if (checkisEmty() == 1) {
 
                 val nt1 = note_1.toFloat()
                 val nt2 = note_2.toFloat()
@@ -72,6 +74,7 @@ class MainActivity : AppCompatActivity() {
                 val nt4 = note_4.toFloat()
 
                 fun check(): Int {
+
 
                     if (!(nt1 >= 0 && nt1 <= 20) || !(nt2 >= 0 && nt2 <= 20) || !(nt3 >= 0 && nt3 <= 20) || !(nt4 >= 0 && nt4 <= 20)) {
                         Toast.makeText(
@@ -82,6 +85,7 @@ class MainActivity : AppCompatActivity() {
                         return 0;
                     }
                     return 1;
+
                 }
 
                 fun calcul_moy(): Float {
@@ -89,16 +93,13 @@ class MainActivity : AppCompatActivity() {
                     return re
                 }
 
-                if (check() == 1)
-                {
+                if (check() == 1) {
                     val moy = calcul_moy()
                     TxtR.text = "votre resultat est : ${moy}"
                     mention(moy)
                 }
-
             }
-
-
+        }
     }
 }
 
